c5358860d9c7cc5e1008765b9214df8cee203d40
Breno Coll <collbreno@dcc.ufrj.br>
2023-10-10 00:54:13 -0300

    Implementing user creation

--- finances/urls.py
+++ finances/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "finances"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("newuser/", views.new_user_form, name="new_user_form"),
+    path("users/", views.UsersView.as_view(), name="users"),
+    path("users/<str:pk>/", views.UserView.as_view(), name="user"),
+    path("add_user/", views.add_user, name="add_user"),
+    path("symbols/", views.symbols, name="symbols"),
+    path("symbols/<str:symbol>", views.symbol, name="symbol")
 ]
--- finances/views.py
+++ finances/views.py
@@ -1,5 +1,46 @@
-from django.http import HttpResponse
+import yfinance as yf
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from requests.exceptions import HTTPError
+from django.urls import reverse
+from django.views import generic
+
+from .models import Person
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the finances index.")
+    return render(request, "finances/index.html")
+
+def symbols(requests):
+    return HttpResponse('lista de simbolos')
+
+def new_user_form(request):
+    return render(request, "finances/new_user_form.html")
+
+def add_user(request):
+    name = request.POST["name"]
+    email = request.POST["email"]
+    person = Person(name=name, email=email)
+    person.save()
+    return HttpResponseRedirect(reverse("finances:users"))
+
+class UsersView(generic.ListView):
+    model = Person
+    template_name = "finances/users.html"
+    context_object_name = "users"
+
+class UserView(generic.DetailView):
+    model = Person
+    template_name = "finances/user.html"
+    context_object_name = 'user'
+
+def symbol(request, symbol):
+    try:
+        ticker = yf.Ticker(symbol)
+    except HTTPError:
+        return HttpResponse(f"{symbol} not found")
+    price = ticker.info["currentPrice"]
+    short_name = ticker.info["shortName"]
+    long_name = ticker.info["longName"]
+    currency = ticker.info["currency"]
+    return HttpResponse(f"vc esta vendo a acao {symbol} \nEmpresa: {long_name}\nValor: {price} {currency}")
