fc7503ada862f054703a5aabc72b97c4bf8454ce
Breno Coll <collbreno@dcc.ufrj.br>
2023-10-16 14:05:16 -0300

    Fixing email connection

--- finances/management/commands/runapscheduler.py
+++ finances/management/commands/runapscheduler.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
 
-from finances.tasks import schedule_database_watcher
+from finances.tasks import schedule_jobs
 
 class Command(BaseCommand):
     help = "Runs APScheduler."
 
     def handle(self, *args, **options):
-        schedule_database_watcher()
+        schedule_jobs()
--- finances/tasks.py
+++ finances/tasks.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler import util
-from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 from django.conf import settings
 from django.core import mail
 from datetime import datetime
@@ -13,21 +13,23 @@ from .utils import download_stock_history, format_email_message
 print('Creating scheduler...')
 scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
 print('Scheduler created!')
-print('Getting mail connection...')
-mail_connection = mail.get_connection()
-print('Opening connection...')
-mail_connection.open()
-print('Connection open!')
 tunnel_id_set = set()
+notification_set = set()
 
-def schedule_database_watcher():
+def schedule_jobs():
     scheduler.add_jobstore(DjangoJobStore(), "default")
     print("Scheduling database watcher")
     scheduler.add_job(
         __watch_database,
-        #TODO: change to 1 minute
-        trigger=CronTrigger(second="*/10"),
+        trigger=CronTrigger(minute="*/1"),
         id='database_watcher',
+        max_instances=1,
+        replace_existing=True,
+    )
+    scheduler.add_job(
+        __send_emails,
+        trigger=CronTrigger(minute="*/1"),
+        id='emails_sender',
         max_instances=1,
         replace_existing=True,
     )
@@ -47,7 +49,7 @@ def __watch_tunnel(tunnel: Tunnel):
         __check_tunnel,
         args=(tunnel,),
         #TODO: change to minutes
-        trigger=CronTrigger(second=f"*/{tunnel.time_interval}"),
+        trigger=CronTrigger(minute=f"*/{tunnel.time_interval}"),
         id=f'tunnel#{tunnel.id}',
         max_instances=1,
         replace_existing=True,
@@ -61,6 +63,7 @@ def __check_tunnel(tunnel: Tunnel):
     history = download_stock_history(stock_symbol)
     stock_datetime = history['datetime'].iloc[-1]
     stock_price = history['price'].iloc[-1]
+    print(f'{stock_symbol} => {stock_price}')
 
     try:
         last_notification_dt = Notification.objects.filter(tunnel=tunnel).latest('datetime').datetime
@@ -77,7 +80,7 @@ def __check_tunnel(tunnel: Tunnel):
                 suggestion = Notification.SELL,
             )
             notification.save()
-            __send_email(notification)
+            notification_set.add(notification)
         elif stock_price < tunnel.min_limit:
             notification = Notification(
                 tunnel = tunnel,
@@ -86,15 +89,21 @@ def __check_tunnel(tunnel: Tunnel):
                 suggestion = Notification.BUY,
             )
             notification.save()
-            __send_email(notification)
+            notification_set.add(notification)
 
-def __send_email(notification: Notification):
-    print(f'Sending email... (tunnel#{notification.tunnel.id})')
-    send_mail(
-        subject="Limite de túnel atingido",
-        from_email=settings.EMAIL_HOST_USER,
-        message=format_email_message(notification),
-        recipient_list=[notification.tunnel.user.email],
-        fail_silently=False,
-        connection=mail_connection
-    )
+def __send_emails():
+    print(f'[{datetime.now().strftime("%H:%M:%S")}] Running task for sending emails')
+    if len(notification_set) != 0:
+        print("Sending emails...")
+        messages = []
+        for notification in notification_set:
+            messages.append((
+                "Limite de túnel atingido",
+                format_email_message(notification),
+                settings.EMAIL_HOST_USER,
+                [notification.tunnel.user.email],
+            ))
+        notification_set.clear()
+        send_mass_mail(messages, fail_silently=False)
+    else:
+        print("No emails to send")
